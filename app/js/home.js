a2e458b1a80a85941c26e05d41534361cb7343b5
Stephen Braitsch <stephen@quietless.com>
2012-06-06 09:34:50 -0700

    building inventory page

--- app/js/home.js
+++ app/js/home.js
@@ -1,5 +1,19 @@
 
 $(document).ready(function(){
+
+	socket = io.connect(); 	
+	var hc = new HomeController();
+		
+})
+
+HomeController = function()
+{
+	var ac; // active collection //
+	var inv = {
+		beds	:{ male	:[50, 100], female	:[50, 100], family:[50, 100] },
+		showers	:{ male	:[50, 100], female	:[50, 100] },
+		meals	:{ bfast:[50, 100], lunch	:[50, 100], dinner:[50, 100] }
+	};
 	
 	$('#btn-logout').click(function(){	
 		console.log('logging out')
@@ -7,5 +21,37 @@ $(document).ready(function(){
 	$('#skt-button').click(function(){
 		socket.emit('bridge-event', { msg:'hello'});
 	})	
-	socket = io.connect();
-})
+	$('#top img').click(function(e){
+		var n = $(e.target).closest('.span4').attr('id');
+		setActiveCollection(n);
+	})
+	$('.opt .image').click(function(e){
+		var d = $(e.target).attr("class");		
+		var n = $(e.target).closest('.span4').attr('id');
+		console.log('clicked', n, d);
+	})
+	$(".opt .slice").hover(handlerIn, handlerOut);
+
+	var handlerIn = function(e)
+	{
+		$(e.currentTarget).fadeTo(200, 1);
+	}
+
+	var handlerOut = function(e)
+	{
+		$(e.currentTarget).fadeTo(200, 0);
+	}	
+
+	var setActiveCollection = function(nc)
+	{
+		$('#'+ac+'-o .opt').each(function(i, o){ $(o).fadeOut(); })
+		$('#'+nc+'-o .opt').each(function(i, o){ $(o).delay(i * 100).fadeIn(); })
+		ac = nc;
+	}
+	
+	var onInventoryClick = function()
+	{
+		// 
+	}	
+	
+}
