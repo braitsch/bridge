b955e33ae550ae37eb5d586e741ab181f3b8c338
Stephen Braitsch <stephen@quietless.com>
2012-07-05 23:17:27 -0700

    adding search / register to clients page

--- app/public/js/home/clients.js
+++ app/public/js/home/clients.js
@@ -1,6 +1,20 @@
 
 $(document).ready(function(){
 	
-	console.log('ok')
+	$('#clients form input').focus();
+	$('#clients form #search').click(function()
+	{
+		return false;
+	})
+	$('#clients form #register').click(function()
+	{
+		return false;
+	})
+
+	function hasGetUserMedia() {
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+	}
+	
+//	console.log(hasGetUserMedia());
 	
 });
