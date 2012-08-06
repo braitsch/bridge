45ad58f6f8bb4bb5e48c21217f20636bda7b0833
Rob Dodson <lets.email.rob@gmail.com>
2012-08-06 14:32:39 -0700

    initial implementation of underscore templated modals

--- app/public/js/dashboard/select-services.js
+++ app/public/js/dashboard/select-services.js
@@ -5,27 +5,46 @@ $(document).ready(function() {
 			url: '/request-services',
 			type: "POST",
 			data: { services : [ {cat:'meals', sub:'breakfast' } ] },
-			success: function(ok) { window.location.href = '/request-provider' },
+			success: function(ok) { window.location.href = '/request-provider'; },
 			error: function(jqXHR){ console.log('error', jqXHR.responseText+' :: '+jqXHR.statusText); }
 		});
 	});
 
-	// Buttons for testing only. Safe to remove if you'd like.
-		$('#btn-timeout').click(function(){ timeoutModal.modal('show'); });
-		$('#btn-reservation-confirm').click(function(){ reservationConfirmModal.modal('show'); });
-		$('#btn-reservation-success').click(function(){ reservationSuccessModal.modal('show'); });
-		$('#btn-service-selection').click(function(){ serviceSelectionModal.modal('show'); });
+	$('.service').click(function() {
+		console.log('clicked!');
 
-		var timeoutModal = $('.modal-timeout');
-		timeoutModal.modal({ show : false, keyboard : true, backdrop : true });
+		// Find the service
+		var serviceName = $(this).attr('id');
+		var service = _.find(services, function(service) { return service.name === serviceName; });
 
-		var reservationConfirmModal = $('.modal-reservation-confirm');
-		reservationConfirmModal.modal({ show : false, keyboard : true, backdrop : true });
-
-		var reservationSuccessModal = $('.modal-reservation-success');
-		reservationSuccessModal.modal({ show : false, keyboard : true, backdrop : true });
-
-		var serviceSelectionModal = $('.modal-service-selection');
+		// Compile the Underscore template for our modal and
+		// pass it the service
+		var tmpl = $('#modal-service-selection').html();
+		var compiled = _.template(tmpl, { service: service });
+		var serviceSelectionModal = $(compiled);
 		serviceSelectionModal.modal({ show : false, keyboard : true, backdrop : true });
+		serviceSelectionModal.modal('show');
+		serviceSelectionModal.on('hidden', function () {
+			$(this).detach();
+		});
+	});
+
+	// Buttons for testing only. Safe to remove if you'd like.
+	$('#btn-timeout').click(function(){ timeoutModal.modal('show'); });
+	$('#btn-reservation-confirm').click(function(){ reservationConfirmModal.modal('show'); });
+	$('#btn-reservation-success').click(function(){ reservationSuccessModal.modal('show'); });
+	$('#btn-service-selection').click(function(){ serviceSelectionModal.modal('show'); });
+
+	var timeoutModal = $('.modal-timeout');
+	timeoutModal.modal({ show : false, keyboard : true, backdrop : true });
+
+	var reservationConfirmModal = $('.modal-reservation-confirm');
+	reservationConfirmModal.modal({ show : false, keyboard : true, backdrop : true });
+
+	var reservationSuccessModal = $('.modal-reservation-success');
+	reservationSuccessModal.modal({ show : false, keyboard : true, backdrop : true });
+
+	// var serviceSelectionModal = $('.modal-service-selection');
+	// serviceSelectionModal.modal({ show : false, keyboard : true, backdrop : true });
 
 });
