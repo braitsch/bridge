1a7cb92fcbb99ec174bc1f46a2b8449b75f24aae
Rob Dodson <lets.email.rob@gmail.com>
2012-08-06 17:09:11 -0700

    remove general from all-services and dummy-data

--- app/server/modules/all-services.js
+++ app/server/modules/all-services.js
@@ -1,11 +1,11 @@
 
 module.exports = [
-	{name : 'housing', fields : ['general', 'individual', 'long-term', 'special-needs']},
-	{name : 'meals', fields : ['general', 'breakfast', 'lunch', 'dinner']},
-	{name : 'health', fields : ['general', 'critical', 'illness', 'ongoing']},
-	{name : 'hygiene', fields : ['general', 'attire', 'shower']},
-	{name : 'employment', fields : ['general', 'resume', 'training']},
-	{name : 'financial', fields : ['general', 'budget']},
-	{name : 'mental', fields : ['general', 'counseling', 'group']},
-	{name : 'transportation', fields : ['general', 'van']}
+	{name : 'housing', fields : ['individual', 'long-term', 'special-needs']},
+	{name : 'meals', fields : ['breakfast', 'lunch', 'dinner']},
+	{name : 'health', fields : ['critical', 'illness', 'ongoing']},
+	{name : 'hygiene', fields : ['attire', 'shower']},
+	{name : 'employment', fields : ['resume', 'training']},
+	{name : 'financial', fields : ['budget']},
+	{name : 'mental', fields : ['counseling', 'group']},
+	{name : 'transportation', fields : ['van']}
 ]
