f3f6c0d93ac7d1f1bceedbcb3bdaad5354cb5774
Stephen Braitsch <stephen@quietless.com>
2012-08-10 10:45:50 -0700

    network-status updates

--- app/public/js/dashboard/network-status.js
+++ app/public/js/dashboard/network-status.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 					service_d.find('.avail').text(service.avail+' / '+service.total);
 					for (var k = service.fields.length - 1; k >= 0; k--){
 						var field = service.fields[k];
-						var field_d = $('#'+service.name+'-'+field.name);
+						var field_d = cnt.find('#'+service.name+'-'+field.name);
 						var field_a = field_d.find('.avail');
 						var field_a_t = field_a.text();
 						var old_avail = parseInt(field_a_t.substr(0, field_a_t.indexOf('/')));
@@ -53,22 +53,25 @@ $(document).ready(function() {
 		// }
 		for (var i = orgs.length - 1; i >= 0; i--){
 			var div = $('#'+orgs[i].name.replace(/\s/g, '-'));
-			console.log('------------');
-			div.find('.service').each(function(k, s){
-				var cat = $(s).find('.cat');
-			//	console.log(cat.attr('id'));
-				var srv = null;
-				for (var n = orgs[i].inv.length - 1; n >= 0; n--){
-					if (orgs[i].inv[n].name == cat.attr('id')) srv = orgs[i].inv[n];
-				};
-				if (srv == null){
+			div.find('.service').each(function(n, s_div){
+				var s_obj = null; // service object //
+				var c_div = $(s_div).find('.cat');
+				for (var n = orgs[i].inv.length - 1; n >= 0; n--) if (orgs[i].inv[n].name == c_div.attr('id')) { s_obj = orgs[i].inv[n]; break; }
+				if (s_obj == null){
 					$(this).css('display', 'none');
 				}	else{
-					
-					$(s).find('.sub').each(function(k, f){
-						for (var j = srv.fields.length - 1; j >= 0; j--){
-							console.log(srv.fields[j]);
-						};
+				// update category avail & total //
+					c_div.find('.avail').text(s_obj.avail+' / '+s_obj.total);
+				// iterate over subcategories //
+					$(s_div).find('.sub').each(function(n, f_div){
+						var f_obj = null;
+						for (var j = s_obj.fields.length - 1; j >= 0; j--) if (s_obj.name+'-'+s_obj.fields[j].name == $(f_div).attr('id')) { f_obj = s_obj.fields[j]; break; }
+						if (f_obj == null){
+							$(this).css('display', 'none');
+						}	else{
+						// update sub-category avail & total //
+							$(f_div).find('.avail').text(f_obj.avail+' / '+f_obj.total);
+						}
 					})
 				}
 			})
