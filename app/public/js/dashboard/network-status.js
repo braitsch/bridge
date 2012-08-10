c514a9f2c69d25de20983eb9f98924be7c6bf44f
Stephen Braitsch <stephen@quietless.com>
2012-08-09 18:27:24 -0700

    network-status updates

--- app/public/js/dashboard/network-status.js
+++ app/public/js/dashboard/network-status.js
@@ -8,30 +8,27 @@ $(document).ready(function() {
 			var s = i > 1 ? ' are '+i+' People ' : ' is '+i+' Person ';
 			$('#connection-count').html('There '+s+' Currently Connected');
 		});
+		var cnt;
 		socket.on('bridge-event', function(org){
-			var oid = org.name.replace(' ', '-');
-			var div = $('#'+oid+' #services');
-				div.empty();
-			var dta = '';
+			cnt = $('#'+org.name.replace(' ', '-')+' #services');
 			for (var i = org.inv.length - 1; i >= 0; i--){
-				var s = org.inv[i];
-				dta += '<div class="service">';
-				dta += '<div class="text">';
-					dta += '<div class="name">'+capitalize(s.name)+'</div>';
-					dta += '<div class="avail">'+s.avail+' / '+s.total+'</div>';
-				dta += '<hr>';
-				dta += '</div>';
-					dta += '<div class="img"><img src="/img/icons/small/icon-'+s.name+'-general.png"></div>';
-				dta += '</div>';
+				var service = org.inv[i];
+				var service_d = $('#'+service.name);
+				service_d.find('.avail').text(service.avail+' / '+service.total);
+				for (var k = service.fields.length - 1; k >= 0; k--){
+					var field = service.fields[k];
+					var field_d = $('#'+service.name+'-'+field.name);
+					var field_a = field_d.find('.avail');
+					var field_a_t = field_a.text();
+					var old_total = field_a_t.substr(field_a_t.indexOf('/')+2);
+					console.log(old_total, field.total, old_total.length, field.total.length, old_total==field.total)
+					if (parseInt(old_total) != parseInt(field.total)){
+						field_d.css('background-color', 'red');
+						field_a.text(field.avail+' / '+field.total);
+					}
+				};
 			}
-			div.append(dta);
 		});
-
-		var aboutModal = $('.modal-about');
-		aboutModal.modal({ show : false, keyboard : true, backdrop : true });
-
-		$('#btn-about').click(function(){ aboutModal.modal('show'); });
-		$('#btn-login').click(function(){ window.location = '/login/'; });
 	}
 
 });
